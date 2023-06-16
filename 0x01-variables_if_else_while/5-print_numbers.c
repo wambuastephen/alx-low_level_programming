9aee3649f0de6a7279f3aff49e32ba1c8e56a261
wambuastephen <wambuastephen97@gmail.com>
2023-06-16 08:01:41 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point to print all strings from 0 - 9
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for num = 0; num = 0; num++)
+printf("%d", num);
+
+printf("\n");
+
+return (0);
+}
