77a612d50196cd7b450abdb7dc6846c69ce7ad18
wambuastephen <wambuastephen97@gmail.com>
2023-06-16 09:46:50 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to print numbers of base 16 in lowercase
+ * Description: This program prints numbers of base 16 (0-9, a-f) in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+char by;
+
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+
+for (by = 'a'; by = 'f'; by++)
+putchar(by);
+
+putchar('\n');
+
+return (0);
+}
