1a106cd78701d63af6833a893538a63d3d14fff5
wambuastephen <wambuastephen97@gmail.com>
2023-06-16 06:25:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib>h>
+#include <time.h>
+
+/**
+ * main - entry to print alphabet lower case.
+ * return: always 0
+ */
+int main(void)
+{
+char bx;
+for (bx = 'a'; bx <= 'z'; bx++)
+{
+putchar(lc);
+}
+putchar('\n");
+return (0);
+}
