1c70ef60852dc415d327521e204587029ea8777d
wambuastephen <wambuastephen97@gmail.com>
2023-06-16 10:39:55 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to print possible combinations of single-digit number
+ * Description:prints all possible ascending combinations of single-digits
+ * commas separates them,  output is followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+{
+for (n = 48; n < 58; n++)
+}
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
