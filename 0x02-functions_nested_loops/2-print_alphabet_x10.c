daa65eed4d20dd681c7d29f0bd66142c3c9f2199
wambuastephen <wambuastephen97@gmail.com>
2023-06-20 06:23:11 -0700

    print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,15 @@
  * Description: This function prints the lowercase alphabet 10 times using
  *              the _putchar function, with each alphabet sequence followed
  *              by a new line character.
- */
-
+ * Return: 0 success
 void print_alphabet_x10(void)
 {
 int ten;
 char bs;
-
 for (ten = 0; ten <= 9; ten++)
 {
 for (bs = 'a'; bs <= 'z'; bs++)
-
-_putchar(la);
+_putchar(bs);
 putchar('\n');
 }
 }
