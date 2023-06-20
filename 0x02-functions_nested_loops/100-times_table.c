c93cb53f8780ada04d14250ceceb525b2d6632f7
wambuastephen <wambuastephen97@gmail.com>
2023-06-20 10:40:00 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,27 +11,28 @@ void print_times_table(int n)
 {
 if (n < 0 || n > 15)
 return;
-
 int i, j, product;
-
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
 product = i * j;
-
 if (j > 0)
+{
+print_single_digit_space(product);
+}
+print_number(product);
+if (j < n)
 {
 _putchar(',');
 _putchar(' ');
-
+}
+}
 if (product < 10)
 _putchar(' ');
-
 if (product < 100)
 _putchar(' ');
 }
-
 if (product < 10)
 _putchar('0' + product);
 else if (product < 100)
