932abf79b52da78cbfba49d113415b9943c24b81
wambuastephen <wambuastephen97@gmail.com>
2023-06-21 07:01:15 -0700

    3-print_remaining_days.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,18 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b)
+if (a > b && a > c)
 {
-if (b > c)
 largest = a;
-else if (a > c)
-largest = a;
-else
-largest = c;
 }
-else
+else if (b > c)
 {
-if (b < c)
-largest = c;
-else
 largest = b;
 }
+else
+{
+largest = c;
+}
+
 return (largest);
 }
