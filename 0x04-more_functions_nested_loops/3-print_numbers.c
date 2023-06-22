2750c363db24c7ebba89e2d3e7be54045ac57308
wambuastephen <wambuastephen97@gmail.com>
2023-06-22 06:13:52 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers- prints numbers between 0 to 9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+int k;
+for (k + 48; ch < 58; k++)
+{
+_putchar(k);
+}
+_putchar('\n');
+}
