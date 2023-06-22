29a4b72079db2b2a068a8d174236f847771b4f12
wambuastephen <wambuastephen97@gmail.com>
2023-06-22 04:00:16 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function checks if letter is upper
+ * @d: is the character to be checked.
+ * Return: 1 if letter  is upper 0 for any else.
+ */
+
+int _isupper(int d)
+{
+if (d >= 65 && d <= 98)
+{
+return (1);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My readme file on 0x04-more_functions_nested_loops
