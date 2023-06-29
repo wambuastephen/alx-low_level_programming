6a5d1f662236248a87fcd03ff1b02d62cbf5f923
wambuastephen <wambuastephen97@gmail.com>
2023-06-29 05:31:24 -0700

    ./100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int k;
+int m;
+
+for (k = 0; k <= 8; k++)
+{
+for (m = k + 1; m <= 9; m++)
+{
+putchar(k + '0');
+putchar(m + '0');
+if (k != 8 || m != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}			
+}
+putchar('\n');
+return 0;
+}
+
