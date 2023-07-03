ed53db8e4dcf3a7bb70e85aec937f3e0fce8deec
wambuastephen <wambuastephen97@gmail.com>
2023-07-03 10:02:38 -0700

    ./100-set_string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -10,22 +10,7 @@
  */
 void set_string(char **s, char *to)
 {
-    *s = to;
+*s = to;
 }
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-char *s0 = "Bob Dylan";
-char *s1 = "Robert Allen";
-
-printf("%s, %s\n", s0, s1);
-set_string(&s1, s0);
-printf("%s, %s\n", s0, s1);
-return (0);
-}
 
