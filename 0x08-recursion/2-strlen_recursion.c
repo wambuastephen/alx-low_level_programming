5ef1a059c383cec028b46bfdffe93076e35bc4c7
wambuastephen <wambuastephen97@gmail.com>
2023-07-05 00:50:21 -0700

    ./4-pow

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,8 +9,8 @@
 int _strlen_recursion(char *s)
 {
 if (*s == '\0') /* Base case: end of string */
-return 0;
+return (0);
 
-return 1 + _strlen_recursion(s + 1); /* Recursively call with next character */
+return (1 + _strlen_recursion(s + 1));/*Recursivelyi call with next character*/
 }
 
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculates the value of x raised to the power of y using recursion.
+ * @x: The base value.
+ * @y: The exponent value.
+ *
+ * Return: The value of x raised to the power of y, or -1 if y is negative.
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0) /* Error case: y is negative */
+return (-1);
+
+if (y == 0) /* Base case: any number raised to 0 is 1 */
+return (1);
+
+return (x * _pow_recursion(x, y - 1)); /* Recursive call to calculate power */
+}
+
