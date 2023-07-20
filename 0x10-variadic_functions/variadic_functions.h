06c5208acab9a9307b2ae9d2cee87b95d3f8f311
wambuastephen <wambuastephen97@gmail.com>
2023-07-20 07:10:53 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,14 +5,13 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 typedef struct print_type
 {
-    char type;
-    void (*print)(va_list args);
+char type;
+void (*print)(va_list args);
 } print_type_t;
 
 void print_all(const char * const format, ...);
