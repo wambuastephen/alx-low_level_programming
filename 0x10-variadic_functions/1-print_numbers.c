feb93d3db65a5b1640eef9d6ed60711af2663dbc
wambuastephen <wambuastephen97@gmail.com>
2023-07-20 23:21:44 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,4 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-
 /**
  * print_numbers - Prints numbers, followed by a new line.
  * @separator: The string to be printed between numbers.
@@ -10,16 +7,19 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list args;
 unsigned int i;
-
+va_list args;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
+if (!separator)
 printf("%d", va_arg(args, int));
-if (separator != NULL && i < n - 1)
-printf("%s", separator);
+else if (separator && i == 0)
+printf("%d", va_arg(args, int));
+else
+printf("%s%d", separator, va_arg(args, int));
 }
-printf("\n");
 va_end(args);
+
+printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,18 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+
 #include <stdio.h>
+#include <stdarg.h>
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
-typedef struct print_type
-{
-char type;
-void (*print)(va_list args);
-} print_type_t;
-
 void print_all(const char * const format, ...);
 
-#endif /* VARIADIC_FUNCTIONS_H */
+#endif
