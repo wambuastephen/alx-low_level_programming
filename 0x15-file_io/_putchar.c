c0944b577dcc73b49f24811b03e57bfff295bb2c
wambuastephen <wambuastephen97@gmail.com>
2023-08-06 22:54:57 -0700

    requsitcat

---
+++ 0x15-file_io/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ *
+ * Return: On success, returns the character written as an unsigned char.
+ *         On failure, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c) {
+return write(1, &c, 1);
+}
