69a08f8e49856126b1c3a33f0a26f1784e0ae59c
wambuastephen <wambuastephen97@gmail.com>
2023-08-06 23:30:53 -0700

    2-append_text_to_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,29 +12,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int file_descriptor; /* Declare the variable at the beginning of the function */
-    ssize_t bytes_written;
+int file_descriptor; /* Declare the variable at the beginning of the function */
+ssize_t bytes_written;
 
-    if (filename == NULL)
-        return (-1);
+if (filename == NULL)
+return (-1);
 
     /* Open file with O_WRONLY | O_CREAT | O_TRUNC flags and 0600 permissions */
-    file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (file_descriptor == -1)
-        return (-1);
-
-    if (text_content != NULL)
-    {
+file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (file_descriptor == -1)
+return (-1);
+if (text_content != NULL)
+{
         /* Write text_content to the file */
-        bytes_written = write(file_descriptor, text_content, strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(file_descriptor);
-            return (-1);
-        }
-    }
-
-    close(file_descriptor);
-    return (1);
+bytes_written = write(file_descriptor, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(file_descriptor);
+return (-1);
+}
 }
 
+close(file_descriptor);
+return (1);
+}
